4f83913d08c260fe5eeb43ddbf8c9801a56b0a57
totto2727 <h_tsuchida@yumemi.co.jp>
2023-07-02 18:25:32 +0900

    chore: ビルド方法の変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [react()],
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: cloudflare({ mode: 'directory' })
 });
