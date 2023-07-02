af8900d07588e2ae44f85c223702f199cf4ad861
totto2727 <h_tsuchida@yumemi.co.jp>
2023-07-02 18:19:44 +0900

    fix: ランタイムのチェックを厳格化

--- src/config/microCMS.ts
+++ src/config/microCMS.ts
@@ -3,23 +3,16 @@ import { getRuntime } from "@astrojs/cloudflare/runtime";
 import { succeed, AnyhowResult, fail } from "@/lib/result";
 
 function loadEnv(env?: Record<string, unknown>) {
-    console.log(env)
     if (!(env && typeof env.MICROCMS_SERVICE_DOMAIN === 'string' && typeof env.MICROCMS_API_KEY === 'string')) throw new Error('MicroCMSの環境変数の読み込みに失敗しました')
     return { serviceDomain: env.MICROCMS_SERVICE_DOMAIN, apiKey: env.MICROCMS_API_KEY }
 }
 
 export type MicroCMSClientObject = ReturnType<typeof createClient>
 
-const isDev = import.meta.env.DEV ?? false
-const isSSR = import.meta.env.SSR ?? false
-
-export function microCMS(request?: Request): MicroCMSClientObject {
-    const isLoadRequest = Boolean(!isDev && isSSR && request)
-    if (!isLoadRequest) {
-        console.log('isLoadRequest:', isLoadRequest)
-        console.log('request:', request)
-    }
-    const env = loadEnv(isLoadRequest ? getRuntime(request ?? new Request('')).env as Record<string, unknown> : import.meta.env);
+export function microCMS(request: Request): MicroCMSClientObject {
+    const runtime = request ? getRuntime(request) as { env?: Record<string, unknown> } | undefined : undefined
+    const isLoadRequest = typeof runtime?.env?.MICROCMS_SERVICE_DOMAIN === 'string' && typeof runtime.env.MICROCMS_API_KEY === 'string'
+    const env = loadEnv(isLoadRequest ? runtime.env : import.meta.env);
     return createClient(env)
 }
 
