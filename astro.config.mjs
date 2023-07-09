48b15937e875d425c7686c2a465bab0b0e257c14
totto2727 <h_tsuchida@yumemi.co.jp>
2023-07-09 18:59:38 +0900

    feat: 画像に関するライブラリの追加とアップデート

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), image()],
   output: "hybrid",
-  adapter: cloudflare({ mode: 'directory' }),
+  adapter: cloudflare({
+    mode: "directory",
+  }),
   vite: {
     define: {
-      'process.env.MICROCMS_SERVICE_DOMAIN': JSON.stringify(process.env.MICROCMS_SERVICE_DOMAIN),
-      'process.env.MICROCMS_API_KEY': JSON.stringify(process.env.MICROCMS_API_KEY)
-    }
-  }
+      "process.env.MICROCMS_SERVICE_DOMAIN": JSON.stringify(
+        process.env.MICROCMS_SERVICE_DOMAIN
+      ),
+      "process.env.MICROCMS_API_KEY": JSON.stringify(
+        process.env.MICROCMS_API_KEY
+      ),
+    },
+  },
 });
