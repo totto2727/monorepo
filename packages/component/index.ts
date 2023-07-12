724133a18dbcb8c86a0bfb41758cb298f4a62e6b
totto2727 <h_tsuchida@yumemi.co.jp>
2023-07-12 19:36:29 +0900

    feat: panda cssの追加

--- packages/component/index.ts
+++ packages/component/index.ts
@@ -2,5 +2,7 @@
 // Then, use this file to export everything you want your user to access.
 
 import MyComponent from './src/MyComponent.astro';
+import Layout from './src/Layout.astro';
+import pandaConfig from './panda.config';
 
-export default MyComponent;
+export { MyComponent, Layout, pandaConfig };
