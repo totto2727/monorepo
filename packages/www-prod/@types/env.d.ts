379ed41cc468492da2fa696d60c6873aaab48875
totto2727 <kaihatu.totto2727@gmail.com>
2023-07-31 10:17:52 +0900

    feat: microcmsの設定

---
+++ packages/www-prod/@types/env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+  readonly MICROCMS_SERVICE_DOMAIN: string;
+  readonly MICROCMS_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
---
+++ packages/www-prod/src/config/microcms.ts
@@ -0,0 +1,14 @@
+import type { MicroCMSClientInstance } from "microcms";
+import { createClient } from "microcms-js-sdk";
+
+export function microCMS(): MicroCMSClientInstance {
+  // if(!import.meta.env.MICROCMS_SERVICE_DOMAIN || !import.meta.env.MICROCMS_API_KEY){
+  //   throw new Error('microCMSの環境変数が設定されていません')
+  // }
+  return createClient({
+    serviceDomain: "totto2727",
+    // serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+    // apiKey: import.meta.env.MICROCMS_API_KEY,
+    apiKey: "2dOGeUJWy17XtKwzTeEQZoDTIuBirZZGColW",
+  });
+}
