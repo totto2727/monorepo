59390ff509e2b469ee280c7af962f67a4d188fab
totto2727 <kaihatu.totto2727@gmail.com>
2023-08-06 12:11:32 +0900

    feat: Cloudflare Adopterを追加

--- app/www-preview/astro.config.mjs
+++ app/www-preview/astro.config.mjs
@@ -1,3 +1,4 @@
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import pandacss from "@pandacss/dev/astro";
 import { defineConfig } from "astro/config";
@@ -6,12 +7,17 @@ import { loadEnv } from "vite";
 // eslint-disable-next-line no-undef
 const env = loadEnv("", process.cwd() + "/../..");
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), pandacss()],
   publicDir: "./node_modules/component/public",
-  server: { port: 3002 },
-  vite: {
-    envDir: env.VITE_LOCAL ? "." : "../../",
+  server: {
+    port: 3002
   },
+  vite: {
+    envDir: env.VITE_LOCAL ? "." : "../../"
+  },
+  output: "server",
+  adapter: cloudflare()
 });
