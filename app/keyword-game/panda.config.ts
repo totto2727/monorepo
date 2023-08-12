f8f3c42f3faa02f8ae89141d3237a37ee857f871
totto2727 <kaihatu.totto2727@gmail.com>
2023-08-12 11:57:08 +0900

    feat: panda cssの設定

--- app/keyword-game/panda.config.ts
+++ app/keyword-game/panda.config.ts
@@ -3,7 +3,4 @@ import { base } from "pandacss-config";
 
 export default defineConfig({
   ...base,
-  include: base.include.concat([
-    "./node_modules/component/src/**/*.{ts,tsx,js,jsx,astro}",
-  ]),
 });
