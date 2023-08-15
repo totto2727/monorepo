9ae8ba2bb36f039f15faa0b9f1c1453b6cbb50ac
totto2727 <kaihatu.totto2727@gmail.com>
2023-08-15 16:56:24 +0900

    fix: Cloudflare Adopterの仕様変更に追従

--- app/www-preview/src/config/microcms.ts
+++ app/www-preview/src/config/microcms.ts
@@ -1,11 +1,11 @@
-import { getRuntime } from "@astrojs/cloudflare/runtime";
 import type { MicroCMSClientInstance } from "microcms";
 import { createClient } from "microcms-js-sdk";
 
-export function microCMS(response: Request): MicroCMSClientInstance {
+// eslint-disable-next-line no-undef
+export function microCMS(locals: App.Locals): MicroCMSClientInstance {
   if (
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    import.meta?.env?.MICROCMS_SERVICE_DOMAIN &&
+    import.meta.env.MICROCMS_SERVICE_DOMAIN &&
     import.meta.env.MICROCMS_API_KEY
   )
     return createClient({
@@ -13,8 +13,10 @@ export function microCMS(response: Request): MicroCMSClientInstance {
       apiKey: import.meta.env.MICROCMS_API_KEY,
     });
 
-  const runtime = getRuntime(response);
-  const env = runtime.env as Record<PropertyKey, string>;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const env = locals.runtime.env as Record<PropertyKey, string>;
   if (!env.MICROCMS_SERVICE_DOMAIN || !env.MICROCMS_API_KEY) {
     throw new Error("microCMSの環境変数が設定されていません");
   }
