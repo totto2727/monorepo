c90952f87f0a5741722022d545e0966d6dd235ae
totto2727 <kaihatu.totto2727@gmail.com>
2023-08-18 02:18:32 +0900

    feat: web analysisを有効化

--- app/keyword-game/src/routes/(base)/layout.tsx
+++ app/keyword-game/src/routes/(base)/layout.tsx
@@ -2,6 +2,7 @@ import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
 import "./base.css";
+import { component$, Slot } from "@builder.io/qwik";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -18,4 +19,19 @@ export const useServerTimeLoader = routeLoader$(() => {
   return {
     date: new Date().toISOString(),
   };
+});
+
+export default component$(() => {
+  return (
+    <>
+      <Slot />
+      {/* Cloudflare Web Analytics */}
+      <script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "3163423d5d3e4e02b2e04187f7f89a37"}'
+      ></script>
+      {/* End Cloudflare Web Analytics */}
+    </>
+  );
 });
