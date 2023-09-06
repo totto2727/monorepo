35f151c318ac1ed93834b12657ae24dc92b16119
totto2727 <kaihatu.totto2727@gmail.com>
2023-09-06 21:18:07 +0900

    feat: コンテンツ用コンポーネントの実装

---
+++ app/www/src/component/ui/blog/index.ts
@@ -0,0 +1,13 @@
+import { default as H1 } from "./H1.astro";
+import { default as H2 } from "./H2.astro";
+import { default as H3 } from "./H3.astro";
+import { default as P } from "./P.astro";
+import { default as Span } from "./Span.astro";
+
+export default {
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  p: P,
+  span: Span,
+};
