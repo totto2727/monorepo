d81f94c8d6687e0e968a891451e5ec33fdf84905
totto2727 <kaihatu.totto2727@gmail.com>
2023-09-06 21:16:18 +0900

    feat: コンテンツコレクションの設定

--- app/www/src/content/config.ts
+++ app/www/src/content/config.ts
@@ -2,17 +2,24 @@ import type { SchemaContext } from "astro:content";
 import { defineCollection, z } from "astro:content";
 
 const tag = z.union([z.literal("implementation"), z.literal("infra")]);
+const status = z.union([
+  z.literal("public"),
+  z.literal("limited"),
+  z.literal("draft"),
+  z.literal("private"),
+]);
 
 const blogScheme = (clx: SchemaContext) =>
   z.object({
     title: z.string(),
-    tags: z
-      .array(tag)
-      .nonempty()
-      .refine((items) => new Set(items).size === items.length, {
-        message: "Must be an array of unique strings",
-      }),
-    eyeCatch: clx.image().optional(),
+    tags: z.array(tag).default([]),
+    eyeCatch: z
+      .object({
+        src: clx.image().optional(),
+        alt: z.string().default(""),
+      })
+      .optional(),
+    status: status.default("private"),
   });
 
 const blogCollection = defineCollection({
