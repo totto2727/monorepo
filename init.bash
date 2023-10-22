77dff39a0711b96c5019bb545c2faaa2196e9bc2
totto2727 <kaihatu.totto2727@gmail.com>
2023-10-22 10:47:48 +0900

    chore: 初期化スクリプトの追加

---
+++ init.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ./package/pandacss-www/
+mkdir -p ./package/pandacss-keyword-game/
+
+echo '{"name": "pandacss-www"}' > ./package/pandacss-www/package.json
+echo '{"name": "pandacss-keyword-game"}' > ./package/pandacss-keyword-game/package.json
